53fb70f20ea630f60d825acc7305b2be3e2c1ecb
Warren Wang <warren.min.wang@gmail.com>
2026-02-12 00:25:05 -0800

    Update UI on first screen, query audio input devices and set the default one as selected in drop down select, update build scripts for debug builds.

--- main.cpp
+++ main.cpp
@@ -1,49 +1,69 @@
 #include <QApplication>
 #include <QLabel>
 #include <QFont>
+#include <QComboBox>
+#include <QGridLayout>
+#include <QPlainTextEdit>
+#include <QAudioDevice>
+#include <QMediaDevices>
 
-// TODO(warren): discover the available audio input devices
-// surely QT has a way to do that talking to the OS
-void query_audio_input_dev()
+#ifdef DEBUG
+#include <QDebug>
+#endif
+
+void init_gui(QWidget *MainWindow, QList<QAudioDevice> *AudioInputDevices)
 {
+    // NOTE(warren): If need multiple screens of buttons/settings, although ideally just put everything on the first screen, 
+    // use multiple layouts I think and a tab/option bar at the top to switch between them.
 
-}
-
-void init_gui(QWidget *MainWindow)
-{
-    int MarginOffset = 10;
-    int SizeInBetweenLabels = 25;
-    int DefaultLabelMinWidth = 150;
-
-    // x,y,width,height
-
-    // TODO(warren): how to get dynamic resizable ui?
+    QGridLayout *GridLayout = new QGridLayout(MainWindow);
     QLabel *AudioSelectLabel = new QLabel("Audio Input", MainWindow);
-    AudioSelectLabel->setGeometry(MarginOffset, 10, DefaultLabelMinWidth, 25);
+    GridLayout->addWidget(AudioSelectLabel, 0, 0);
 
-    // TODO(warren): dropdown QComboBox to select audio input devices
+    QComboBox *AudioInputSelect = new QComboBox(MainWindow);
+    QString DefaultAudioDeviceDescription = QMediaDevices::defaultAudioInput().description();
+    QString CurrentAudioDeviceDescription;
+    for (int i = 0; i < AudioInputDevices->size(); i++)
+    {
+        QString Description = AudioInputDevices->at(i).description();
+        AudioInputSelect->addItem(Description);
+        if (DefaultAudioDeviceDescription == Description)
+        {
+            AudioInputSelect->setCurrentIndex(i);
+        }
+    }
+    AudioInputSelect->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+    // TODO(warren): Need to attach a function that listens to and sets the selected value
+    // on the AudioInputSelect, storing that into a state we maintain at a higher level somewhere...global state?
+    GridLayout->addWidget(AudioInputSelect, 1, 0);
 
     QLabel *ModelSelectLabel = new QLabel("STT Model", MainWindow);
-    ModelSelectLabel->setGeometry(MarginOffset + SizeInBetweenLabels + DefaultLabelMinWidth,
-                                  10,
-                                  DefaultLabelMinWidth,
-                                  25);
+    GridLayout->addWidget(ModelSelectLabel, 2, 0);
+    QComboBox *ModelSelect = new QComboBox(MainWindow);
+    ModelSelect->addItem("TODO");
+    GridLayout->addWidget(ModelSelect, 3, 0);
 
     QLabel *InferenceDeviceSelectLabel = new QLabel("Inference Device", MainWindow);
-    InferenceDeviceSelectLabel->setGeometry(MarginOffset + SizeInBetweenLabels + (2*DefaultLabelMinWidth),
-                                            10,
-                                            DefaultLabelMinWidth,
-                                            25);
+    GridLayout->addWidget(InferenceDeviceSelectLabel, 4, 0);
+    QComboBox *InferenceDeviceSelect = new QComboBox(MainWindow);
+    InferenceDeviceSelect->addItem("TODO");
+    GridLayout->addWidget(InferenceDeviceSelect, 5, 0);
 
     QLabel *RawTextOutputLabel = new QLabel("Raw Text Output", MainWindow);
-    RawTextOutputLabel->setGeometry(MarginOffset,
-                                    100,
-                                    DefaultLabelMinWidth,
-                                    25);
+    GridLayout->addWidget(RawTextOutputLabel, 6, 0);
+    QPlainTextEdit* RawTextOutputTextBox = new QPlainTextEdit("TODO", MainWindow);
+    RawTextOutputTextBox->setReadOnly(true);
+    GridLayout->addWidget(RawTextOutputTextBox, 7, 0);
 }
 
 int main(int argc, char *argv[])
 {
+    // TODO(warren): Need some thinking on the arch, need to initialize a local model first, etc.
+    // Although, we should query the machine for specs and try to determine if any model we will have
+    // the open weights for will be able to load/run at all. If not, do run a window and show an error
+    // message...suggesting need better specs and provide a yolo button to allow trying to run on potatoes.
+    // ex. query_machine_specs()
+    
     QApplication App(argc, argv);
 
     QFont Font("Georgia");
@@ -57,10 +77,17 @@ int main(int argc, char *argv[])
 
     // TODO(warren): Maybe just have a global main chunk of
     // mem to store stuff, some kinda global allocator, arena allocator?
+    QList<QAudioDevice> AudioInputDevices = QMediaDevices::audioInputs();
+#ifdef DEBUG
+    qDebug() << "Available Audio Input Devices:" << AudioInputDevices.count();
+    qDebug() << "Default Audio Input device:" << QMediaDevices::defaultAudioInput().description();
+    for (const QAudioDevice &device : AudioInputDevices)
+    {
+        qDebug() << "Audio Input Devices:" << device.description();
+    }
+#endif
 
-    // query_audio_input_dev
-
-    init_gui(&MainWindow);
+    init_gui(&MainWindow, &AudioInputDevices);
 
     MainWindow.show();
     return App.exec();
