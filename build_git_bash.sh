9351b10640be39ee3e59919955adf0b87ac0503f
Warren Wang <warren.min.wang@gmail.com>
2026-03-16 00:56:59 -0700

    Enable GGML_CUDA in the build system
    Add VOICETYPER_CUDA cmake option (default OFF) that enables GGML_CUDA for whisper.cpp.
    
    Update build_git_bash.sh to accept a cuda flag.
    
    Add post-build step to copy CUDA runtime DLLs when enabled.

--- build_git_bash.sh
+++ build_git_bash.sh
@@ -2,13 +2,17 @@
 
 export CMAKE_PREFIX_PATH="C:/Qt/6.10.2/msvc2022_64"
 BUILD_TYPE="Release"
+CUDA_FLAG=""
 
-if [ "$1" == "debug" ]; then
-    BUILD_TYPE="Debug"
-fi
+for arg in "$@"; do
+	case "$arg" in
+		debug) BUILD_TYPE="Debug" ;;
+		cuda)  CUDA_FLAG="-DVOICETYPER_CUDA=ON" ;;
+	esac
+done
 
 mkdir -p build
 cd build
-cmake .. -G "Visual Studio 17 2022" -A x64
+cmake .. -G "Visual Studio 17 2022" -A x64 $CUDA_FLAG
 cmake --build . --config "$BUILD_TYPE"
 cd ..
