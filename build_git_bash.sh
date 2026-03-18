6a1f3414fc91bcd4fc57b09dba8d3ee46ee9b669
Warren Wang <warren.min.wang@gmail.com>
2026-03-17 22:42:51 -0700

    Fix CUDA build: set CUDA_PATH and handle bin/x64 DLL path
    Set CUDA_PATH env var in build script for MSBuild CUDA targets.
    
    Check for bin/x64/ subdirectory when locating CUDA runtime DLLs.

--- build_git_bash.sh
+++ build_git_bash.sh
@@ -2,17 +2,21 @@
 
 export CMAKE_PREFIX_PATH="C:/Qt/6.10.2/msvc2022_64"
 BUILD_TYPE="Release"
-CUDA_FLAG=""
+EXTRA_FLAGS=()
 
 for arg in "$@"; do
 	case "$arg" in
 		debug) BUILD_TYPE="Debug" ;;
-		cuda)  CUDA_FLAG="-DVOICETYPER_CUDA=ON" ;;
+		cuda)
+			export CUDA_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v13.2"
+			EXTRA_FLAGS+=("-DVOICETYPER_CUDA=ON")
+			EXTRA_FLAGS+=("-DCUDAToolkit_ROOT=$CUDA_PATH")
+			;;
 	esac
 done
 
 mkdir -p build
 cd build
-cmake .. -G "Visual Studio 17 2022" -A x64 $CUDA_FLAG
+cmake .. -G "Visual Studio 17 2022" -A x64 "${EXTRA_FLAGS[@]}"
 cmake --build . --config "$BUILD_TYPE"
 cd ..
