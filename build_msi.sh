a288ff644fb16430280012e7f76b860d55ccbcc7
Warren Wang <warren.min.wang@gmail.com>
2026-03-24 17:45:36 -0700

    Fix MSI build: pass ProductVersion variable to WiX
    Read version from VERSION file and pass it as a preprocessor variable to the wix build command.

--- build_msi.sh
+++ build_msi.sh
@@ -4,10 +4,11 @@ set -e
 
 BUILD_OUTPUT="build/Release"
 MSI_OUTPUT="build/VoiceTyper.msi"
+PRODUCT_VERSION=$(cat VERSION | tr -d '[:space:]')
 
 echo ""
 echo "=== Building MSI installer ==="
-wix build -o "$MSI_OUTPUT" -d "BuildOutput=$(pwd)/$BUILD_OUTPUT" packaging/VoiceTyper.wxs
+wix build -o "$MSI_OUTPUT" -d "BuildOutput=$(pwd)/$BUILD_OUTPUT" -d "ProductVersion=$PRODUCT_VERSION" packaging/VoiceTyper.wxs
 
 echo ""
 echo "=== Done ==="
