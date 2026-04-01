d8ada06981e7c10c04e09b2e58cb1d57fcc63337
Warren Wang <warren.min.wang@gmail.com>
2026-04-01 00:54:00 -0700

    Separate build directories for Qt and ImGui builds
    ImGui builds now use build-imgui/ to avoid stale Qt DLLs from previous Qt builds polluting the output directory.

--- build_git_bash.sh
+++ build_git_bash.sh
@@ -13,14 +13,19 @@ for arg in "$@"; do
 	esac
 done
 
+BUILD_DIR="build"
+if [ "$USE_IMGUI" = "ON" ]; then
+	BUILD_DIR="build-imgui"
+fi
+
 EXTRA_FLAGS=("-DVOICETYPER_CUDA=$USE_CUDA" "-DVOICETYPER_USE_IMGUI=$USE_IMGUI")
 if [ "$USE_CUDA" = "ON" ]; then
 	export CUDA_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v13.2"
 	EXTRA_FLAGS+=("-DCUDAToolkit_ROOT=$CUDA_PATH")
 fi
 
-mkdir -p build
-cd build
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
 cmake .. -G "Visual Studio 17 2022" -A x64 "${EXTRA_FLAGS[@]}"
 cmake --build . --config "$BUILD_TYPE"
 cd ..
@@ -30,6 +35,6 @@ if [ "$USE_CUDA" = "ON" ]; then
 	if [ -d "$CUDA_PATH/bin/x64" ]; then
 		CUDA_DLL_DIR="$CUDA_PATH/bin/x64"
 	fi
-	cp -u "$CUDA_DLL_DIR"/cublas64_*.dll "build/$BUILD_TYPE/"
-	cp -u "$CUDA_DLL_DIR"/cublasLt64_*.dll "build/$BUILD_TYPE/"
+	cp -u "$CUDA_DLL_DIR"/cublas64_*.dll "$BUILD_DIR/$BUILD_TYPE/"
+	cp -u "$CUDA_DLL_DIR"/cublasLt64_*.dll "$BUILD_DIR/$BUILD_TYPE/"
 fi
